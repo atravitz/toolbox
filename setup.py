4062bd3d37627784ed033fef4920bd267b5bc30c
Alyssa Travitz <atravitz@umich.edu>
2019-08-28 11:48:44 -0400

    adding license and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toolbox",
+    version="0.0.1",
+    author="Alyssa Travitz",
+    author_email="atravitz@umich.edu",
+    description="helpful tools for research",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atravitz/toolbox.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License : OSI Approved :: MIT License",
+    ]
+)
